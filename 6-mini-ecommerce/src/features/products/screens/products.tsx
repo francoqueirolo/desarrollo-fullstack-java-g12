181c88614db389449aa4142a5222efa192caf9fc
Marcelo <marcelo@Marcelos-MacBook-Pro.local>
2025-12-10 22:49:34 -0500

    mini-ecommerce update in products page

--- 6-mini-ecommerce/src/features/products/screens/products.tsx
+++ 6-mini-ecommerce/src/features/products/screens/products.tsx
@@ -1,3 +1,21 @@
+import type { Product } from "@/common/types";
+import { useEffect, useState } from "react";
+import { ProductCard } from "../components/product-card/product-card";
+
 export function Products() {
-  return <div>Products page</div>;
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <div className="p-6 flex justify-center gap-4 flex-wrap w-full">
+      {products.map((product) => (
+        <ProductCard product={product} key={product.id} />
+      ))}
+    </div>
+  );
 }
